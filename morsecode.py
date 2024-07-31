a68d16e99df0fb7fa6facf090f243f512e15e2d8
ajanya03 <130043690+ajanya03@users.noreply.github.com>
2024-07-31 15:30:14 +0530

    Update morsecode.py
    Solved KeyError

--- morsecode.py
+++ morsecode.py
@@ -20,9 +20,10 @@ def morse_code_generator():
 
     while True:
         user_input = input("Enter your alphabet to get morse code or enter (0) to exit : ").upper()
-        morse_code = morse_code_list[user_input]
-        print("Corresponding morse code \n ",user_input," = ",morse_code)
-    if user_input == '0':
-        print("Thank you for using our system :) ")
-
+        if user_input == '0':
+            print("Thank you for using our system :) ")
+        else:
+            morse_code = morse_code_list[user_input]
+            print("Corresponding morse code \n ",user_input," = ",morse_code)
+   
 morse_code_generator()
